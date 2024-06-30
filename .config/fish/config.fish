549b582367a6a2cb58cefb0ce0b1cef06e79a6d7
Philo-W <philo_wu@outlook.com>
2024-06-30 20:17:16 +1000

    Update gitignore

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/phil_oh/anaconda3/bin/conda
-    eval /Users/phil_oh/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-end
-# <<< conda initialize <<<
-
---
+++ README.md
@@ -0,0 +1,13 @@
+# Phi's /.(dot)files Backup
+
+## What do I use:
+
+<table>
+<tr>
+    <th>terminal</th>
+    <td>alacritty</td>
+</tr>
+<tr>
+    <th>editor</th>
+    <td>neovim</td>
+</tr>
