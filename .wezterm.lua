2631e52d888ba67cd7e27b6fa3d015de66c3ce72
Philo Wu <philo_wu@outlook.com>
2024-09-07 17:28:33 +1000

    update wezterm: removed bg blur

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,11 +5,11 @@ local config = {}
 local act = wezterm.action
 
 config.font = wezterm.font("Liga SFMono Nerd Font", { weight = "Bold" })
-config.font_size = 18
+config.font_size = 23
 config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = false
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 80
+-- config.window_background_opacity = 0.8
+-- config.macos_window_background_blur = 80
 config.window_decorations = "RESIZE"
 config.mouse_bindings = {
 	{
