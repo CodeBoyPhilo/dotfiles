f4090cdff4010f26a6243015e44515d1b9aa5f7e
Philo Wu <philo_wu@outlook.com>
2024-11-19 00:57:13 +1100

    update bufferline: enhance appearance

--- .config/nvim/lua/philo/plugins/bufferline.lua
+++ .config/nvim/lua/philo/plugins/bufferline.lua
@@ -1,11 +1,23 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {
+		options = {
+			mode = "tabs",
+			indicator = { style = "icon", icon = "▎" },
+			separator_style = { "", "" },
+			buffer_close_icon = "",
+			modified_icon = "●",
+			close_icon = "",
+			left_trunc_marker = "",
+			right_trunc_marker = "",
+			max_name_length = 20,
+			max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+			tab_size = 22,
+			show_buffer_close_icons = false,
+			show_close_icon = false,
+			always_show_bufferline = true,
+		},
+	},
 }
