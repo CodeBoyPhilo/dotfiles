9d49c8dfeb5b384e9f73550f7e45910d8ca6809e
Philo Wu <philo_wu@outlook.com>
2024-12-03 17:48:54 +1100

    update lsp: add back python formatter

--- .config/nvim/lua/philo/plugins/lsp/mason.lua
+++ .config/nvim/lua/philo/plugins/lsp/mason.lua
@@ -35,11 +35,11 @@ return {
 
 		mason_tool_installer.setup({
 			ensure_installed = {
-				-- "prettier", -- prettier formatter
+				"prettier", -- prettier formatter
 				-- "stylua", -- lua formatter
-				-- "isort", -- python formatter
-				-- "black", -- python formatter
-				-- "pylint", -- python linter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint", -- python linter
 				-- "eslint_d", -- js linter
 			},
 		})
