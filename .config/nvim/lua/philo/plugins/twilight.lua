fddc7364485432d1fc7ea1128b968dfd51b8635a
Philo Wu <philo_wu@outlook.com>
2024-12-24 14:24:41 +1100

    fix (nvim): reduce twilight dimming lines

--- .config/nvim/lua/philo/plugins/twilight.lua
+++ .config/nvim/lua/philo/plugins/twilight.lua
@@ -2,7 +2,7 @@ return {
 	"folke/twilight.nvim",
 	opts = {
 		dimming = {
-			alpha = 0.8, -- amount of dimming
+			alpha = 0.6, -- amount of dimming
 		},
 		context = 12, -- amount of lines we will try to show around the current line
 	},
