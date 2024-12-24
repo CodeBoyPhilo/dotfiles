6f8a6f94c1266c4eb524b3f9ef27378ed9e9e1bc
Philo Wu <philo_wu@outlook.com>
2024-12-24 14:25:27 +1100

    fix (sketchybar): change bar color scheme to black and white

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
 # Color Palette
+# export BLACK=0xff181926
+# export WHITE=0xffcad3f5
+# export RED=0xffed8796
+# export GREEN=0xffa6da95
+# export BLUE=0xff8aadf4
+# export YELLOW=0xffeed49f
+# export ORANGE=0xfff5a97f
+# export MAGENTA=0xffc6a0f6
+# export GREY=0xff939ab7
+# export TRANSPARENT=0x00000000
 export BLACK=0xff181926
 export WHITE=0xffcad3f5
-export RED=0xffed8796
-export GREEN=0xffa6da95
-export BLUE=0xff8aadf4
-export YELLOW=0xffeed49f
-export ORANGE=0xfff5a97f
-export MAGENTA=0xffc6a0f6
-export GREY=0xff939ab7
+export RED=0xffcad3f5
+export GREEN=0xffcad3f5
+export BLUE=0xffcad3f5
+export YELLOW=0xffcad3f5
+export ORANGE=0xffcad3f5
+export MAGENTA=0xff181926
+export GREY=0xff181926
 export TRANSPARENT=0x00000000
 
 # General bar colors
-export BAR_COLOR=0xa024273a
+export BAR_COLOR=0x00000000
 export ICON_COLOR=$WHITE # Color of all icons
 export LABEL_COLOR=$WHITE # Color of all labels
 export BACKGROUND_1=0x903c3e4f
@@ -22,7 +32,7 @@ export BACKGROUND_2=0x90494d64
 export POPUP_BACKGROUND_COLOR=0xff24273a
 export POPUP_BORDER_COLOR=$WHITE
 
-export SHADOW_COLOR=$BLACK
+export SHADOW_COLOR=$WHITE
 
 export BATTERY_1=0xffa6da95
 export BATTERY_2=0xffeed49f
