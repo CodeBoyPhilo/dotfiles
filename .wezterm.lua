d98448903c38a36d90244828279c61a68a8f4533
Philo Wu <philo_wu@outlook.com>
2024-12-24 14:25:44 +1100

    feat (wezterm): add background image support

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,9 +7,12 @@ local act = wezterm.action
 config.font = wezterm.font("Liga SFMono Nerd Font", { weight = "Bold" })
 config.font_size = 23
 config.color_scheme = "Catppuccin Mocha"
+--
 config.enable_tab_bar = false
--- config.window_background_opacity = 0.8
--- config.macos_window_background_blur = 80
+-- config.window_background_opacity = 0
+-- config.macos_window_background_blur = 0
+config.window_background_image = "/Users/phil_oh/wezterm_bg_image.png"
+
 config.window_decorations = "RESIZE"
 config.mouse_bindings = {
 	{
